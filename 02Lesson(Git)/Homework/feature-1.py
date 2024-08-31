e60260f73c88fa7a8d2d92db249145fc48e67a2b
Illia Savchenko <ilas70587@gmail.com>
2024-08-31 20:26:09 +0200

    Resolve conflict feature-1

---
+++ 02Lesson(Git)/Homework/feature-1.py
@@ -0,0 +1,11 @@
+import shutil
+import os
+
+def move_files(src, dst):
+    for filename in os.listdir(src):
+        shutil.move(os.path.join(src, filename), dst)
+
+source_folder = 'Top secret'
+destination_folder = 'My-server'
+
+move_files(source_folder, destination_folder)
