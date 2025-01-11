f9310f7beac24eb6723206e0211a488fe4258590
Illia Savchenko <54515152+KastonI@users.noreply.github.com>
2025-01-11 16:44:30 +0100

    Lecture 17 (#10)
    * Add files with homework 17
    
    * Update README.md
    
    * Update Dockerfile
    
    * Update Dockerfile

---
+++ Lecture_17/multi-container-app/init/init.sql
@@ -0,0 +1,14 @@
+\c devops;
+CREATE TABLE IF NOT EXISTS test (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    pass VARCHAR(100) NOT NULL,
+    phone VARCHAR(15) NOT NULL
+);
+
+INSERT INTO test (name, pass, phone)
+VALUES
+('Oleksandr', 'cm105nv53bds', '+3800000000'),
+('Denys', 'mfk3v02mvh5', '+3800000001'),
+('Artem', '32ndk4v92', '+3800000002'),
+('Nazarii', 'cn38vfm23ld0', '+3800000003');
